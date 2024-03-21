9abc2bc7afac515b62ffd4361e7fede88695e03b
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2024-03-21 12:58:01 -0300

    chore: install and config jest, supertest, cors and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
+import 'dotenv/config';
+
+import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(helmet());
 
-const port = process.env.PORT || 4568;
+const port = process.env.APP_PORT || 4568;
 
 app.get('/ping', (req, res) => {
   return res.send('pongs');
